6a93057bdd1aa19c7f1ba63ac4939ab1fff07659
Олександр Дибач <alex.dybach.2014@i.ua>
2020-10-22 21:01:04 +0300

    Done lesson 001 task 2

---
+++ Less001_Task2/Less001_Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Less001_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int birthyear = 2000;
+            Console.WriteLine(birthyear);
+        }
+    }
+}
